05ada400ba3fa63cdd2bdb353cda6dec893243f8
ahmad afiquddin <ahmad_afiquddin@hotmail.com>
2019-03-04 10:26:10 +1100

    Added models

--- stesite/models.py
+++ stesite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Steganography(models.Model):
+	carrier = models.ImageField(upload_to="gallery", default="")
+	payload = models.ImageField(upload_to="gallery", default="")
+	retImg = models.ImageField(upload_to="gallery", default="")
+
